5ad0b7e2754a51e3df612ee0f8c14553b9c1421b
hoborox2 <43296441+hoborox2@users.noreply.github.com>
2018-10-06 10:14:38 -0300

    de volta a origem
    xd

--- fila.h
+++ fila.h
@@ -1,65 +1,29 @@
 #ifndef _FILA_F
 #define _FILA_H
-
-#include "tokenizacao.h"
-
-
-typedef struct fila
-{
-	Fila* fila_criar();
-	Fila *f = (Fila*) malloc(sizeof(Fila));
-	f->primeiro = NULL;
-	f->ultimo = NULL;
-	return f;
-} Fila;
-
-void fila_adicionar(Fila *f, Token t)
-{
-	No *n = (No*) malloc(sizeof(No));
-	n->t = t;
-	n->prox = NULL;
-	if(f->fim == NULL) 
-	{
-		f->primeiro = n;
-		f->ultimo = n;
-		return;
-	}
-	f->ultimo->prox = n;
-	f->ultimo = n;
-};
-
-Token fila_remover(Fila *f)
-{
-	No *excluir = f->inicio;
-	f->inicio = f->inicio->prox;
-	if(f->inicio == NULL) 
-	{
-		f->fim = NULL;
-	}
-	Token = excluir->t;
-	free(excluir);
-	return info;
-};
-
+ #include "tokenizacao.h"
+ typedef struct fila Fila;
+ /*
+ * Cria uma nova fila vazia.
+ */
+Fila* fila_criar();
+ /*
+ * Adiciona um elemento ao fim da fila.
+ */
+void fila_adicionar(Fila *f, Token t);
+ /*
+ * Remove um elemento do início da fila.
+ */
+Token fila_remover(Fila *f);
+ /*
+ * Retorna 1 se a fila estiver vazia e 0 caso contrário.
+ */
 int fila_vazia(Fila *f);
-{
-	if(f->size==0)
-	{
-		printf("A fila esta vazia!\n");
-		return 1;
-	}
-};
-return 0;
-
+ /*
+ * Destrói a fila e seus elementos.
+ */
 void fila_destruir(Fila *f);
-
-/*
+ /*
  * Imprime os elementos da fila.
  */
-void fila_imprimir(Fila *f)
-{
-	printf("%d\n", primeiro->Fila);
-	primeiro = primeiro->ultimo;
-};
-
-#endif
+void fila_imprimir(Fila *f);
+ #endif
