b69955331b1d71e366e0d01e43b4511ef7511c10
hoborox2 <43296441+hoborox2@users.noreply.github.com>
2018-10-06 10:28:42 -0300

    pilha pronta

--- pilha.c
+++ pilha.c
@@ -2,70 +2,82 @@
 #include <stdlib.h>
 #include "pilha.h"
 
-typedef struct no {
+typedef struct no 
+{
   Token token;
   struct no *prox;
 } No;
 
-struct pilha {
+struct pilha 
+{
   No *primeiro;
 };
 
-Pilha* pilha_criar() {
+Pilha* pilha_criar() 
+{
 	Pilha *p = (Pilha*) malloc(sizeof(Pilha));
 	p->primeiro = NULL;
 	return p;
-
 }
 
-void pilha_push(Pilha *p, Token t) {
+void pilha_push(Pilha *p, Token t) 
+{
 	No *n = (No*) malloc(sizeof(No));
 	n->token = t;
 	n->prox = p->primeiro;
 	p->primeiro = n;
 }
 
-Token pilha_pop(Pilha *p) {
+Token pilha_pop(Pilha *p) 
+{
 	Token  t;
-	if(p->primeiro == NULL) {
+	if(p->primeiro == NULL) 
+	{
 	printf("Pilha vazia\n");
-	return t;}
+	return t;
+	}
 	
 	No *tmp = p->primeiro;
 	t = tmp->token;
 	p->primeiro = tmp->prox;
 	free(tmp);
-
 	return t;
 }
 
-Token pilha_primeiro(Pilha *p) {
+Token pilha_primeiro(Pilha *p) 
+{
 	return p->primeiro->token;  
 }
 
-int pilha_vazia(Pilha *p) {
-	if(p->primeiro == NULL) {
+int pilha_vazia(Pilha *p) 
+{
+	if(p->primeiro == NULL) 
+	{
 	printf("pilha vazia!\n");
-	return 0;}
-
+	return 1;
+	} else {
+		return 0;
+	}
 }
 
-void pilha_destruir(Pilha *p) {
-
+void pilha_destruir(Pilha *p) 
+{
 	No *tmp = p->primeiro;
-	while(tmp != NULL) {
-	No *excluir = tmp;
-	tmp = tmp->prox;
-	free(excluir);
+	while(tmp != NULL) 
+	{
+		No *excluir = tmp;
+		tmp = tmp->prox;
+		free(excluir);
 	}
 	free(p);
-
 }
 
-void pilha_imprimir(Pilha *p) {
+void pilha_imprimir(Pilha *p) 
+{
 	No *tmp = p->primeiro;
-
-	while(tmp != NULL) {
+	while(tmp != NULL) 
+	{
 	token_imprimir(tmp->token);
-	tmp = tmp->prox;}
+	tmp = tmp->prox;
+	}
 }
