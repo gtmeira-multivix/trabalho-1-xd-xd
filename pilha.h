df00ac2ed69ffc68140b3df8bae95bd768909aaa
hoborox2 <43296441+hoborox2@users.noreply.github.com>
2018-10-06 09:46:55 -0300

    alteração da pilha

--- pilha.h
+++ pilha.h
@@ -3,41 +3,104 @@
 
 #include "tokenizacao.h"
 
-typedef struct pilha Pilha;
+typedef struct pilha 
+{
+	double *dados;
+	int tamanho;
+	int topo;
+} Pilha;
 
 /*
  * Cria uma nova pilha vazia.
  */
-Pilha* pilha_criar();
+Pilha* pilha_criar(int tamanho)
+{
+	Pilha *p = (Pilha*) malloc(sizeof(Pilha));
+	p->topo = 0;
+	p->dados = (double*) malloc(tamanho*sizeof(double));
+	p->tamanho = tamanho;
+	return p;
+}
 
 /*
  * Adiciona elemento no topo da pilha.
  */
-void pilha_push(Pilha *p, Token t);
+void pilha_push(Pilha *p, Token t)
+{
+	if(p->topo >= p->tamanho) 
+	{
+		printf("Pilha cheia!\n");
+		return;
+	}
+	p->dados[p->topo] = t;
+	p->topo++
+}
 
 /*
- * Retira elemento do início da pilha.
+ * Retira elemento do inÃ­cio da pilha.
  */
-Token pilha_pop(Pilha *p);
+Token pilha_pop(Pilha *p)
+{
+	if(p->topo == 0) 
+	{
+		printf("Pilha vazia!\n");
+		return 0;
+	}
+	p->topo--;
+	return p->dados[p->topo];
+}
 
 /*
- * Retorna o primeiro elemento da pilha, sem retirá-lo.
+ * Retorna o primeiro elemento da pilha, sem retirÃ¡-lo.
  */
-Token pilha_primeiro(Pilha *p);
+Token pilha_primeiro(Pilha *p)
+{
+	p->topo;
+	return p->dados[p->topo];
+}
 
 /*
- * Retorna 1 se a pilha estiver vazia ou 0 caso contrário.
+ * Retorna 1 se a pilha estiver vazia ou 0 caso contrÃ¡rio.
  */
-int pilha_vazia(Pilha *p);
+int pilha_vazia(Pilha *p)
+{
+
+   if(p->topo==NULL)
+   {
+      return 1;
+   }
+   else
+   {
+	return 0;
+	}
+}
 
 /*
- * Destrói a pilha e seus elementos.
+ * DestrÃ³i a pilha e seus elementos.
  */
-void pilha_destruir(Pilha *p);
+void pilha_destruir(Pilha *p)
+{
+	free(p->dados);
+	free(p)
+}
 
 /*
  * Imprime todos os elementos na pilha atualmente.
  */
-void pilha_imprimir(Pilha *p);
+void pilha_imprimir(Pilha *p)
+{
+    int i;
+    
+    if(top==-1)
+    {
+        printf("\nA pilha esta vazia!\n");
+    }
+    else
+    {
+        printf("\nOs elementos da pilha sao:\n");
+        for(i=topo;i>=0;--i)
+            printf("%d\n\t",pilha[i]);
+    }
+}
 
 #endif
